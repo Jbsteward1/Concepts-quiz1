9567530622dc895e6dcf94b5677804137cf55599
Jeremy <jbsteward1@gmail.com>
2021-08-31 10:24:56 -0400

    1st and 2nd half of concepts quiz.

---
+++ Popquiz.py
@@ -0,0 +1,33 @@
+
+PART I
+Navigating the File System
+1.  How do you move back and forth between directories? To move back and forth through a directory you (pwd) print working drectory to list what already on computer then (cd) change directory based on what you have stored.
+
+2.  How do you create and delete files? to create a file you can (touch then add file name.(something)), to delete a file you (rm then add the file name.)
+
+3.  How do you create and delete directories? to create a directory you first (cd ..) to the desktop then type (mkdir) add new directory(folder) name,  to delete a directory you would (rmdir) along with the directory name.
+
+4.  How can I see exactly where I am within the filesystem? To find where you are you could (pwd) print working directory. 
+
+5.  How can I see the files and directories within the current directory that I am in? The (ls) to list the current directories.
+
+
+
+Git
+1.  How do you let git know you want to track changes made in a directory? You would a initiliaze a git repo by typing (git init).
+
+2.  How do you tell git that you like the work you’ve done and want it to be a part of a chunk of work to be saved? To let git know i like my work i would use the (get add) option along with the file name.
+
+3.  How do you tell git you want to save a chunk of work? To actually save that piece of work i would then use a (git commit -m) alone with a descriptive message put into qotations. 
+
+4.  How do you tell git where to push your local changes up to a online repository? You would use the (git push) code and it will then give you a direction of where to send the push upstream.
+
+5.  How can you safely work on a different version of your current project? To work on a different version of the current project you make a new branch using (git branch) and add a name behind it.
+
+6.  How can you make that different version become a part of your main version? To make a different version a part of the main branch you would merge the branch using (git merge) and since you mightve been on the new branch you would add it to main.
+
+7.  How do I move between versions of my project? To move between versions you would use (git checkout) along wit the name you want to switch to.
+
+8.  How can I go back to previous chunks of work that I’ve created and work from that point in time? To go back you could go itnot the git repo and edit or from the terminal you could (git revert) to the point you would wanna edit, it keeps the histoty but removes the contents.
+
+9.  Let’s say I wrote a bad message for a commit. How could I change it? To change a bad message for a commit i would use (git commit -- amend) then the new message.
